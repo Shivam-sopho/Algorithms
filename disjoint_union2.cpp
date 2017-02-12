73212cacd71bbff7fdb840531294bd39ee0ae4c2
shivam <chotu.kv@gmail.com>
2017-02-12 07:06:24 +0530

    Commit-2

--- disjoint_union2.cpp
+++ disjoint_union2.cpp
@@ -10,13 +10,13 @@ class disunion{
     public:
 		vector<ll> size;
 		vector <ll> v;
-		disunion(n);// Utility function to carry out initialization process
+		disunion(ll n);// Utility function to carry out initialization process
         bool find(ll a,ll b);// Utility function to check the that 2 numbers are from same set or not
         ll root( ll a);// Find the root of the current element
-		void union( ll a , ll b);// Utility function to carry out the union process
+		void union1( ll a , ll b);// Utility function to carry out the union process
 };
-disunion ::disunion(n)
-{		
+disunion ::disunion(ll n)
+{
 	size.resize(n+1);
 	v.resize(n+1);
 	for(ll i = 0 ; i <= v.size(); i++)
@@ -38,22 +38,22 @@ ll disunion :: root(ll a){
 	}
     return a;
 }
-void disunion :: union(ll a, ll b){
+void disunion :: union1(ll a, ll b){
 	ll x = root(a);
 	ll y = root(b);
-	if(size(x) < size(y)){
-		root(x) = root(y);
-		size(y)+=size(x);
+	if(size[x] < size[y]){
+		v[root(x)] = v[root(y)];
+		size[y]+=size[x];
 	}
 	else{
-		root(y) = root(x);
-		size(x)+=size(y);
+		v[root(y)] = v[root(x)];
+		size[x]+=size[y];
 	}
 }
 int main()
 {
-	 
 
-	
+
+
 }
 
